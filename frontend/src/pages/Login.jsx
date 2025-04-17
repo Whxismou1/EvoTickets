0547fae961af852a55ae049bc1d0a3e0db7ff74b
Whxismou1 <melouc00@estudiantes.unileon.es>
2025-04-17 19:45:13 +0200

    hotfix: SigIn With Google repair config

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,8 +12,6 @@ import { login } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-import {signInWithGoogle} from "../config/firebase.config";
-
 export default function LoginPage() {
   const { t } = useTranslation();
   const navigate = useNavigate();
@@ -170,7 +168,10 @@ export default function LoginPage() {
               <Button
                 type="button"
                 variant="outline"
-                onClick={signInWithGoogle}
+                onClick={() => {
+                  console.log("Google login clicked");
+                }
+                }
                 className="w-full border-[#A28CD4] text-[#5C3D8D] hover:bg-[#A28CD4]/10 mb-4"
               >
                 <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
