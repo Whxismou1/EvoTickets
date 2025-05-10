a0b36c719250e701207c9a351a3e14237be6ce5c
Cristian <fenis357@gmail.com>
2025-05-10 20:44:50 +0200

    feat: changes applied after syncing with main

--- backend/src/main/java/com/evotickets/controllers/UserController.java
+++ backend/src/main/java/com/evotickets/controllers/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
